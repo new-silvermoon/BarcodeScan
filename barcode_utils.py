b6078cd0c0ccc785e85ca44ba2a2a9f6710230cc
Sagar Das <faith.frontiers@gmail.com>
2019-07-23 13:34:45 +0530

    Added Decode logic
    Added barcode decode logic

---
+++ barcode_utils.py
@@ -0,0 +1,73 @@
+
+L_dict = {
+    "0001101": 0,
+    "0011001": 1,
+    "0010011": 2,
+    "0111101": 3,
+    "0100011": 4,
+    "0110001": 5,
+    "0101111": 6,
+    "0111011": 7,
+    "0110111": 8,
+    "0001011": 9
+}
+
+G_dict = {
+    "0100111": 0,
+    "0110011": 1,
+    "0011011": 2,
+    "0100001": 3,
+    "0011101": 4,
+    "0111001": 5,
+    "0000101": 6,
+    "0010001": 7,
+    "0001001": 8,
+    "0010111": 9
+}
+R_dict = {
+    "1110010": 0,
+    "1100110": 1,
+    "1101100": 2,
+    "1000010": 3,
+    "1011100": 4,
+    "1001110": 5,
+    "1010000": 6,
+    "1000100": 7,
+    "1001000": 8,
+    "1110100": 9
+}
+
+first_six_digits = {
+    1: L_dict,
+    2: G_dict,
+    3: L_dict,
+    4: G_dict,
+    5: G_dict,
+    6: L_dict
+}
+
+def decode(bar_widths):
+    global L_dict,G_dict,R_dict,first_six_digits
+
+    #Decoding
+    count = 0
+    for i in range(3, len(bar_widths),4):
+        count+=1
+        widths = bar_widths[i:i+4]
+        if count <= 6:
+            code = ""
+            for j,val in enumerate(widths):
+                code += '1' * val if (j+1) % 2 ==0 else '0'*val
+            mode_dict = first_six_digits.get(count)
+
+            print(code)
+            print(mode_dict.get(code))
+        else:
+            code = ""
+            for j, val in enumerate(widths):
+                code += '0' * val if (j + 1) % 2 == 0 else '1' * val
+            print(code)
+            print(R_dict.get(code))
+
+
+
---
+++ main.py
@@ -0,0 +1,113 @@
+import numpy as np
+import cv2
+from collections import Counter
+from barcode_utils import decode
+import math
+
+
+def grab_contours(cnts):
+
+    if len(cnts) == 2:
+        cnts = cnts[0]
+
+
+    elif len(cnts) == 3:
+        cnts = cnts[1]
+
+    else:
+        raise Exception(("Contours tuple must have length 2 or 3"))
+
+    # return the actual contours array
+    return cnts
+
+def scan():
+    #reading image
+    path='data/code1.png'
+    image = cv2.imread(path)
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+    #computing the Scharr gradient magnitude representation of the images in both x and y direction
+    gradX = cv2.Sobel(gray,ddepth=cv2.CV_32F,dx=1, dy=0,ksize=-1)
+    gradY = cv2.Sobel(gray,ddepth=cv2.CV_32F, dx = 0, dy = 1, ksize=-1)
+
+    #subtract the y-gradient from the x-gradient
+    '''Performing this subtraction creates regions with high horizontal gradients
+    and low vertical gradients'''
+    gradient = cv2.subtract(gradX,gradY)
+    gradient = cv2.convertScaleAbs(gradient)
+
+
+    '''Ignore noise and focus only on barcode region
+    '''
+    #Blurring using a 9x9 kernel for smoothing out high frequency noise
+
+    ''' Thresholding to binary image. Any value less than 255 is made 0 (black)
+    otherwise white
+    '''
+    blurred = cv2.blur(gradient, (9, 9))
+    _, img_bin = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY)
+
+    # construct a closing kernel and apply it to the thresholded image
+    '''Creating a rectangular kernel with width greater than height. Helps
+    in closing the vertical gaps within the barcodes'''
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
+    #Applying morphological operation to close the gaps within the barcode
+    closed = cv2.morphologyEx(img_bin, cv2.MORPH_CLOSE, kernel)
+
+    #Performing erosion and dilation to remove unnecessary blobs
+    closed = cv2.erode(closed,None,iterations=4)
+    closed = cv2.dilate(closed, None, iterations=4)
+
+    # find the contours in the binary image, then sort the contours
+    # by their area, keeping only the largest one
+    cnts = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = grab_contours(cnts)
+    c = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
+
+    # compute the rotated bounding box of the largest contour
+    rect = cv2.minAreaRect(c)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    min_vals = np.amin(box,axis=0)
+    max_vals = np.amax(box,axis=0)
+
+    """Cropping the barcode region"""
+    crop_image = image[min_vals[1]:min_vals[1]+20, min_vals[0]:max_vals[0]]
+    img_gray = cv2.cvtColor(crop_image, cv2.COLOR_BGR2GRAY)
+    """Enhancing contrast"""
+    crop_image_new = cv2.convertScaleAbs(img_gray, alpha=1.5, beta=0)
+    """Converting barcode to binary image"""
+    ret, bin_img = cv2.threshold(crop_image_new, 200, 255, cv2.THRESH_BINARY)
+
+    """Creating ideal binary image"""
+    rows_count,cols_count = bin_img.shape
+
+    for i in range(cols_count):
+        col_vals = bin_img[:,i]
+        ct_vals = Counter(col_vals)
+        bin_img[:,i] = 255 if ct_vals[255] > ct_vals[0] else 0
+        
+
+    #TODO: Try with canny edges
+
+    width_list = []
+    prev_val = bin_img[0][0]
+    count = 1
+    for val in bin_img[0]:
+        if val == prev_val:
+            count+=1
+        else:
+            width_list.append(count)
+            count=1
+            prev_val = val
+
+    width_list = list(map(lambda x: int(math.ceil(x/width_list[2])),width_list))
+    decode(width_list)
+
+    cv2.drawContours(image, [box], -1, (0, 255, 0), 3)
+    cv2.imshow("Image", image)
+    cv2.imshow("Bin Image ", bin_img)
+    cv2.waitKey(0)
+
+scan()
